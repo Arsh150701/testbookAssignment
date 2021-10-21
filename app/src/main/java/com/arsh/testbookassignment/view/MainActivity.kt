08774b0f41197891d9a39e4b0ef12b7ac787a906
Akshat Srivastava <akshatkakul@gmail.com>
2021-10-22 01:11:57 +0530

    first commit

---
+++ app/src/main/java/com/arsh/testbookassignment/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arsh.testbookassignment.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arsh.testbookassignment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
